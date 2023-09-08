e594751bb7e944639cb60886f2faf00a86ca182d
victorrossh <victor.ross098@gmail.com>
2023-09-08 19:47:06 -0300

    teste

--- stats/error.php
+++ stats/error.php
@@ -1,3 +1,19 @@
 <div class="content text-center mt-5">
     <h1>Page Not Found</h1> 
 </div>
+
+<div class="content text-center mt-5">
+    <h1>Page Not Found</h1> 
+</div>
+
+<div class="content text-center mt-5">
+    <h1>Page Not Found</h1> 
+</div>
+
+<div class="content text-center mt-5">
+    <h1>Page Not Found</h1> 
+</div>
+
+<div class="content text-center mt-5">
+    <h1>Page Not Found</h1> 
+</div>
