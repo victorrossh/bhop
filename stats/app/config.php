c9c03806c411a23a2a04b6ea056f218356b83854
victorrossh <victor.ross098@gmail.com>
2023-09-08 19:43:24 -0300

    commit inicial

---
+++ stats/app/config.php
@@ -0,0 +1,7 @@
+<?php
+// ===== MySQL SETTINGS ========================================================
+define("DB_HOST", "127.0.0.1");
+define("DB_USER", "ogpuser");
+define("DB_PASS", "Victorsql@pro-runners2023");
+define("DB_NAME", "ogp_panel");
+define("DB_TYPE", "mysql");
